40af520f3fe698897562b6da6189fee5e17ddb32
Sarah <sarahrudy@gmail.com>
2021-11-13 14:26:54 -0700

    Update API call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,11 +5,12 @@ export const fetchAllRecipes = () => {
   .catch(error => console.log(error))
 }
 
-// export const fetchRecipeByIngredients = (userIngredients) => {
-//   return fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userIngredients}&app_id=b3d28c55&app_key=3572fecb4b04d1e24475822a52a414b9`)
-//   .then(response => checkError(response))
-//   .catch(error => console.log(error))
-// }
+export const fetchRecipesByIngredients = (userIngredients) => {
+  return fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userIngredients}&app_id=b3d28c55&app_key=3572fecb4b04d1e24475822a52a414b9&diet=low-carb&time=0-30`)
+  .then(response => checkError(response))
+  .then(response => response.hits)
+  .catch(error => console.log(error))
+}
 
 const checkError = (response) => {
   if (!response.ok) {
