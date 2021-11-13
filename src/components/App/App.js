319dac8c56bfb7ff47875f51db1bb285da4385d8
Sarah <sarahrudy@gmail.com>
2021-11-13 14:41:01 -0700

    Switch App to functional component for hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import './App.css'
-import { fetchAllRecipes } from '../../apiCalls'
 import NavBar from '../NavBar/NavBar'
 import Recipes from '../Recipes/Recipes'
-import Dropdown from '../Dropdown/Downdown'
+import Dropdown from '../Dropdown/Dropdown'
+import { fetchRecipesByIngredients } from '../../apiCalls'
+import { useHistory } from 'react-router-dom'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      recipes: []
-    }
-  }
+const App = () => {
+  const history = useHistory()
+  const [ searchedRecipes, setSearchedRecipes ] = useState([])
+  const [ userIngredients, setUserIngredients ] = useState(null)
 
-  componentDidMount() {
-    fetchAllRecipes()
-    .then(recipes => this.setState({ recipes: recipes })) 
-  }
+  useEffect(() => {
+    if(userIngredients) {
+    fetchRecipesByIngredients(userIngredients)
+      .then(data => setSearchedRecipes(data)) }
+  }, [userIngredients])
   
-  render() {
+  const handleSubmit = (ingredient1, ingredient2, ingredient3) => {
+    setUserIngredients(`${ingredient1}%20${ingredient2}%20${ingredient3}`)
+    history.push('/recipes')
+  }
+
     return (
       <main className="app">
         <NavBar />
-        <Dropdown />
-        <Route exact path='/' render={() => <Recipes recipes={ this.state.recipes } /> } /> 
+        <Dropdown handleSubmit={ handleSubmit } /> 
+        <Recipes searchedRecipes={ searchedRecipes } /> 
       </main>
     )
   }
-}
+
 
 export default App
