6f177e7192312f488501a589baa015ed0c848f1b
Sarah <sarahrudy@gmail.com>
2021-11-13 14:43:35 -0700

    Add searchedRecipes as prop

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from 'react'
 import './Recipes.css'
 import RecipeCard from '../RecipeCard/RecipeCard'
 
-const Recipes  = ({ recipes }) => {
-  console.log(recipes)
-  const recipeCard = recipes.map(recipe => {
+const Recipes  = ({ searchedRecipes }) => {
+  const recipeCard = searchedRecipes.map((recipe, i) => {
     return (
       <div className='recipes'>
         <RecipeCard 
+          key={ i }
           label={ recipe.recipe.label }
           image={ recipe.recipe.image }
+          // add onClick here to take user to url of recipe on web
         />
       </div>
     )
