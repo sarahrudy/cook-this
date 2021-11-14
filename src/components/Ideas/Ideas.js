35dce168de4c3a91447d43e2c97fe25e6ac2e8cf
Sarah <sarahrudy@gmail.com>
2021-11-14 15:50:34 -0700

    Add Links and onClick to food images

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,15 +1,36 @@
-import React from 'react' 
-import { NavLink } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import smoothieImg from '../../images/smoothie.png'
+import saladImg from '../../images/salad.png'
+import chickenImg from '../../images/chicken.png'
+import pastaImg from '../../images/pasta.png'
+import healthyImg from '../../images/diet.png'
 
-const Ideas = () => {
+const Ideas = ({ handleImageClick }) => {
   return (
-    <section>
+    <section className='ideas'>
       <h2>Need some ideas?</h2>
-        <NavLink to='/smoothies'>Smoothies</NavLink>
-        <NavLink to='/salads'>Salads</NavLink>
-        <NavLink to='/chicken'>Chicken Dishes</NavLink>
-        <NavLink to='/pasta'>Pasta Dishes</NavLink>
-        <NavLink to='/healthy'>Healthy Dishes</NavLink>
+        <Link to='/recipes'>
+          <img src={ smoothieImg } className='ideas-img' alt=''
+            onClick={ () => handleImageClick('smoothie') }>
+          </img>
+        </Link>
+        <Link to='/recipes'>
+          <img src={ saladImg } className='ideas-img' alt='' 
+            onClick={ () => handleImageClick('salad') }></img>
+        </Link>
+        <Link to='/recipes'>
+          <img src={ chickenImg } className='ideas-img' alt=''
+            onClick={ () => handleImageClick('chicken') }></img>
+        </Link>
+        <Link to='/recipes'>
+          <img src={ pastaImg } className='ideas-img' alt='' 
+            onClick={ () => handleImageClick('pasta') }></img>
+        </Link>
+        <Link to='/recipes'>
+          <img src={ healthyImg } className='ideas-img' alt='' 
+            onClick={ () => handleImageClick('healthy') }></img>
+        </Link>
     </section>
   );
 };
