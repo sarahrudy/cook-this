86c96dd1f65d7b395a299756d4f61fd9ad02ce21
Sarah <sarahrudy@gmail.com>
2021-11-14 16:04:15 -0700

    Add some styling

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Ideas.css'
 import { Link } from 'react-router-dom'
 import smoothieImg from '../../images/smoothie.png'
 import saladImg from '../../images/salad.png'
@@ -8,27 +9,27 @@ import healthyImg from '../../images/diet.png'
 
 const Ideas = ({ handleImageClick }) => {
   return (
-    <section className='ideas'>
+    <section className='ideas-container'>
       <h2>Need some ideas?</h2>
         <Link to='/recipes'>
-          <img src={ smoothieImg } className='ideas-img' alt=''
+          <img src={ smoothieImg } className='ideas-img' alt='smoothie'
             onClick={ () => handleImageClick('smoothie') }>
           </img>
         </Link>
         <Link to='/recipes'>
-          <img src={ saladImg } className='ideas-img' alt='' 
+          <img src={ saladImg } className='ideas-img' alt='salad bowl' 
             onClick={ () => handleImageClick('salad') }></img>
         </Link>
         <Link to='/recipes'>
-          <img src={ chickenImg } className='ideas-img' alt=''
+          <img src={ chickenImg } className='ideas-img' alt='chicken'
             onClick={ () => handleImageClick('chicken') }></img>
         </Link>
         <Link to='/recipes'>
-          <img src={ pastaImg } className='ideas-img' alt='' 
+          <img src={ pastaImg } className='ideas-img' alt='pasta' 
             onClick={ () => handleImageClick('pasta') }></img>
         </Link>
         <Link to='/recipes'>
-          <img src={ healthyImg } className='ideas-img' alt='' 
+          <img src={ healthyImg } className='ideas-img' alt='healthy ingredients' 
             onClick={ () => handleImageClick('healthy') }></img>
         </Link>
     </section>
