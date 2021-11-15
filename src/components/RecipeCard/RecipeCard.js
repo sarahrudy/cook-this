35c93edcfbaa9de5e8cc2242cac1b3cecb5b6092
Sarah <sarahrudy@gmail.com>
2021-11-14 21:42:45 -0700

    Revert recipe card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,32 +1,12 @@
-import React, { useState } from 'react'
-import Recipes from '../Recipes/Recipes'
+import React from 'react'
 import './RecipeCard.css'
 
 const RecipeCard = ({ label, image }) => {
-  const [ hover, setHover ] = useState(false)
-
-  const handleMouseEnter = () => {
-    setHover(true)
-  }
-
-  const handleMouseLeave = () => {
-    setHover(false)
-  }
-
   return (
     <div className='recipe-card'>
       <article className='recipe-card-container'>
-        {hover 
-        ?
-        <h1>hi</h1>
-        :
-        <>
         <p className='recipe-card-title'>{ label }</p>
-        <img className='recipe-card-image' src={ image } alt='recipe'
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        ></img> 
-        </> }
+        <img className='recipe-card-image' src={ image } alt='recipe'></img>
       </article>
     </div>
   )
